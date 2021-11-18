07c9a8539906c96352797323cf656196ac117b2b
Shawn Koh <shawn@shawnkoh.sg>
2021-11-19 07:30:19 +0800

    F

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import * as mqtt from "mqtt";
 import * as dotenv from "dotenv";
+import Active from "./Active";
 
 dotenv.config();
 
@@ -23,24 +22,7 @@ const App = () => {
     });
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Active />;
 };
 
 export default App;
