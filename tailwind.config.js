17a02e2606b9f6e8e490f6f1c2e1afca13c6057c
Shawn Koh <shawn@shawnkoh.sg>
2021-11-19 07:42:54 +0800

    Add official plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/aspect-ratio")
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
   ],
 }
